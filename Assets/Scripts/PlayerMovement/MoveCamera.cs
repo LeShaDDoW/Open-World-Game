2525de1458ad7d72113bc80917c201e598a9ddb3
LeShaDDoW <92387799+LeShaDDoW@users.noreply.github.com>
2022-06-23 18:31:01 +0200

    some more post processing

--- Assets/Scripts/PlayerMovement/MoveCamera.cs
+++ Assets/Scripts/PlayerMovement/MoveCamera.cs
@@ -5,6 +5,7 @@ public class MoveCamera : MonoBehaviour
     [Tooltip("If F5 changes view!")] public bool switchableView;
     [Tooltip("The Head of the player where the camera gets placed!")] public Transform player;
     [Tooltip("How far the camera away is in third person!")] public float distance;
+    [Tooltip("The Main Camera Object!")] public Transform mainCamera;
 
     bool firstPerson = true;
 
@@ -13,10 +14,7 @@ public class MoveCamera : MonoBehaviour
         if(switchableView == true && firstPerson == true && Input.GetKeyDown(KeyCode.F5))
         {
             firstPerson = false;
-            Debug.Log("hehehehah");
-        }
-
-        if (switchableView == true && firstPerson == false && Input.GetKeyDown(KeyCode.F5))
+        } else if (switchableView == true && firstPerson == false && Input.GetKeyDown(KeyCode.F5))
         {
             firstPerson = true;
         }
@@ -28,7 +26,14 @@ public class MoveCamera : MonoBehaviour
 
         if (firstPerson == false)
         {
-            transform.position = new Vector3(player.position.x, player.position.y, player.position.z + distance);
+            transform.position = player.transform.position;
+
+            RaycastHit hit;
+            Physics.Raycast(transform.position, transform.forward, out hit, distance * 0.01f);
+
+            if(hit.collider == null) mainCamera.position = new Vector3(0, 0, -distance);
+            else mainCamera.position = new Vector3(transform.position.x, 
+                transform.position.y, hit.point.z);
         }
     }
 }
